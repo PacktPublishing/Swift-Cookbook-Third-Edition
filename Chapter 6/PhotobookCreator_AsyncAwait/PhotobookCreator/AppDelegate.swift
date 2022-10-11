421c25807fd48ccc0f4dde9735a2f44582685b72
Danny Bolella <dbolella@gmail.com>
2022-10-10 20:19:41 -0400

    Add async/await recipe

---
+++ Chapter 6/PhotobookCreator_AsyncAwait/PhotobookCreator/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  PhotobookCreator
+//
+//  Created by Danny Bolella on 10/4/22.
+//  Copyright © 2022 Danny Bolella. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
