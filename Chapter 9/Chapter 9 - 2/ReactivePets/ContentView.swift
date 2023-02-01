980366c802bb940c644e661f28a9becf3538cc4f
Danny Bolella <dbolella@gmail.com>
2023-02-01 16:26:20 -0500

    Recipe 2

---
+++ Chapter 9/Chapter 9 - 2/ReactivePets/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  ReactivePets
+//
+//  Created by Daniel Bolella on 2/1/23.
+//
+
+import SwiftUI
+
+class ObservablePet : ObservableObject {
+    @Published var name = ""
+    @Published var age = ""
+    @Published var breed = ""
+    
+    init (){ }
+}
+
+struct ContentView: View {
+    @StateObject var pet = ObservablePet()
+    
+    var body: some View {
+        VStack(alignment: .center){
+            Text("Your Pets Name is: \(pet.name)")
+            Text("Your Pets Age is: \(pet.name)")
+            Text("Your Pets Breed is: \(pet.name)")
+            
+            TextField("Enter name", text: $pet.name)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            TextField("Enter age", text: $pet.age)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            TextField("Enter breed", text: $pet.breed)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            Divider()
+            FetchView(petDownStream: pet)
+        }
+        .padding()
+    }
+}
+
+struct FetchView: View {
+    @ObservedObject var petDownStream: ObservablePet
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            Text("\(petDownStream.name), a \(petDownStream.age) year old \(petDownStream.breed) will fetch the 🎾!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
